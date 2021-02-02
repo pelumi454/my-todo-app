0bb076e3466b553b9bc4ce7a20bb598c36e7ea14
pelumi454 <amoochristianah454@gmail.com>
2021-02-02 21:57:33 +0000

    button

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './HomePage.css'
+import {Link} from 'react-router-dom'
 import image2 from '../Images/blobbs.jpg'
 import image3 from '../Images/blob3.svg'
 import Navbar from "./Navbar";
@@ -15,7 +16,9 @@ const HomePage = () => {
             <div className="homepage-text">
             <h2 className="homepage-h2">Welcome to iTodos</h2>
             <p className="homepage-p">infinitely flexible. incredibly easy to use.</p>
+            <Link to='/SignUp'>
             <button className="button">Get Started</button>
+            </Link>
             </div>
           
             
